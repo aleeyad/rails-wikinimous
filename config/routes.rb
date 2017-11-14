4ecba0e72b5161639b2d6012196a758d5363708a
Aleeya Dasgupta <aleeya@gmail.com>
2017-11-14 17:13:06 +0100

    incomplete wiki something exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  # get 'articles/index'
+
+  # get 'articles/show'
+
+  # get 'articles/create'
+
+  # get 'articles/new'
+
+  # get 'articles/update'
+
+  # get 'articles/edit'
+
+  # get 'articles/destroy'
+resources :articles
+root to: 'articles#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
